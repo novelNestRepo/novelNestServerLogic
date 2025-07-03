97c6cde9989136dd50b56ccc263f8794f8784bb2
Youssef-joe <joe92228@gmail.com>
2025-07-04 00:46:33 +0300

    cleaning the environment

--- real-time-service/src/models/channel.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
-
-export const channels = pgTable("channels", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 255 }).notNull(),
-  created_by: uuid("created_by").notNull(),
-  created_at: timestamp("created_at").defaultNow().notNull(),
-});
--- real-time-service/src/routes/channelRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createChannel, joinChannel } from "../controllers/channelController";
-
-const router = Router();
-
-router.post("/", createChannel); // POST /channels
-router.post("/:channelId/join", joinChannel); // POST /channels/:channelId/join
-
-export default router;
